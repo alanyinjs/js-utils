36b7fedfb83a3be52fbd66a2bb8058afcab10213
Alan Yin <alanyinjs@gmail.com>
2018-10-12 00:27:25 +1100

    added immediate argument

--- debounce.js
+++ debounce.js
@@ -1,21 +1,30 @@
-export default function debounce(fn, wait) {
+export default function debounce(fn, wait, immediate=false) {
   var timeout;
   var result;
 
   var debounced = function(){
     var context = this;
     var args = arguments;
+    var callNow = immediate && !timeout;
 
     clearTimeout(timeout);
     timeout = setTimeout(function() {
-      result = fn.apply(context, args);
+      timeout = null;
+      if(!immediate){
+        result = fn.apply(context, args);
+      }
     }, wait);
+
+    if(callNow) {
+      result = fn.apply(context, args);
+    }
 
     return result;
   };
 
   debounced.cancel = function() {
     clearTimeout(timeout);
+    timeout = null;
   };
 
   return debounced;
